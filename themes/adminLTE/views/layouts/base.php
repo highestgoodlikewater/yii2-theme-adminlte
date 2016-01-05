6da3529eb55991734825a0d54347aa51b274d9a0
eakkabinz <eakkabin_it@outlook.co.th>
2016-01-05 10:37:59 +0700

    Update 2016-01-05 10:37

--- themes/adminLTE/views/layouts/base.php
+++ themes/adminLTE/views/layouts/base.php
@@ -45,48 +45,43 @@
                         </a>
                         <div class="navbar-custom-menu">
                             <ul class="nav navbar-nav">
-                                <li class="dropdown user user-menu">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                        <img src="/backend/images/user2-160x160.jpg" class="user-image" alt="User Image">
-                                        <?php if(\Yii::$app->user->isGuest): ?>
-                                            <span class="hidden-xs">Guest</span>
-                                        <?php else: ?>
+                                <?php if(\Yii::$app->user->isGuest): ?>
+									<li>
+                                        <a href="#"><span class="hidden-xs">Guest</span></a>
+                                    </li>
+								<?php else: ?>
+                                    <li class="dropdown user user-menu">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                            <img src="/backend/images/user2-160x160.jpg" class="user-image" alt="User Image">
                                             <span class="hidden-xs"><?php echo ucfirst(Yii::$app->user->identity->username); ?></span>
-                                        <?php endif; ?>
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li class="user-header">
-                                            <img src="/backend/images/user2-160x160.jpg" class="img-circle" alt="User Image">
-                                            <?php if(\Yii::$app->user->isGuest): ?>
-                                                <p>Guest - Administration<small>Member since Nov. 2012</small></p>
-                                            <?php else: ?>
-                                                <p><?php echo ucfirst(Yii::$app->user->identity->username); ?> - Administration<small>Member since Nov. 2012</small></p>
-                                            <?php endif; ?>
-                                        </li>
-                                        <li class="user-body">
-                                            <div class="col-xs-4 text-center">
-                                                <a href="#">Followers</a>
-                                            </div>
-                                            <div class="col-xs-4 text-center">
-                                                <a href="#">Sales</a>
-                                            </div>
-                                            <div class="col-xs-4 text-center">
-                                                <a href="#">Friends</a>
-                                            </div>
-                                        </li>
-                                        <li class="user-footer">
-                                            <div class="pull-left">
-                                                <a href="#" class="btn btn-default btn-flat">Profile</a>
-                                            </div>
-                                            <div class="pull-right">
-                                                <a href="#" class="btn btn-default btn-flat">Sign out</a>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#"><i class="fa fa-gears"></i></a>
-                                </li>
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li class="user-header">
+                                                <img src="/backend/images/user2-160x160.jpg" class="img-circle" alt="User Image">
+                                                <p><?php echo ucfirst(Yii::$app->user->identity->username); ?> - Administration<small>Member since Nov. 2015</small></p>
+                                            </li>
+                                            <li class="user-body">
+                                                <div class="col-xs-4 text-center">
+                                                    <a href="#">Followers</a>
+                                                </div>
+                                                <div class="col-xs-4 text-center">
+                                                    <a href="#">Sales</a>
+                                                </div>
+                                                <div class="col-xs-4 text-center">
+                                                    <a href="#">Friends</a>
+                                                </div>
+                                            </li>
+                                            <li class="user-footer">
+                                                <div class="pull-left">
+                                                    <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                                </div>
+                                                <div class="pull-right">
+                                                    <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                <?php endif; ?>
                             </ul>
                         </div>
                     </nav>
@@ -114,63 +109,66 @@
                             <li class="active treeview">
                                 <a href="/backend"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
                             </li>
-                            <li class="treeview">
-                                <a href="#">
-                                    <i class="fa fa-files-o"></i>
-                                    <span>Layout Options</span>
-                                    <span class="label label-primary pull-right">4</span>
-                                </a>
-                                <ul class="treeview-menu">
-                                    <li><a href="#"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
-                                    <li><a href="#"><i class="fa fa-circle-o"></i> Boxed</a></li>
-                                    <li><a href="#"><i class="fa fa-circle-o"></i> Fixed</a></li>
-                                    <li><a href="#"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <i class="fa fa-th"></i> <span>Widgets</span> <small class="label pull-right bg-green">new</small>
-                                </a>
-                            </li>
-                            <li class="treeview">
-                                <a href="#">
-                                    <i class="fa fa-pie-chart"></i><span>Charts</span><i class="fa fa-angle-left pull-right"></i>
-                                </a>
-                                <ul class="treeview-menu">
-                                    <li><a href="#"><i class="fa fa-circle-o"></i> ChartJS</a></li>
-                                    <li><a href="#"><i class="fa fa-circle-o"></i> Morris</a></li>
-                                    <li><a href="#"><i class="fa fa-circle-o"></i> Flot</a></li>
-                                    <li><a href="#"><i class="fa fa-circle-o"></i> Inline charts</a></li>
-                                </ul>
-                            </li>
-                            <li class="treeview">
-                                <a href="#">
-                                    <i class="fa fa-share"></i> <span>Multilevel</span>
-                                    <i class="fa fa-angle-left pull-right"></i>
-                                </a>
-                                <ul class="treeview-menu">
-                                    <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
-                                    <li>
-                                        <a href="#"><i class="fa fa-circle-o"></i> Level One <i class="fa fa-angle-left pull-right"></i></a>
-                                        <ul class="treeview-menu">
-                                            <li><a href="#"><i class="fa fa-circle-o"></i> Level Two</a></li>
-                                            <li>
-                                                <a href="#"><i class="fa fa-circle-o"></i> Level Two <i class="fa fa-angle-left pull-right"></i></a>
-                                                <ul class="treeview-menu">
-                                                    <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
-                                                    <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+                            <?php if(!\Yii::$app->user->isGuest): ?>
+                                <li class="treeview">
+                                    <a href="#">
+                                        <i class="fa fa-files-o"></i>
+                                        <span>Layout Options</span>
+                                        <span class="label label-primary pull-right">4</span>
+                                    </a>
+                                    <ul class="treeview-menu">
+                                        <li><a href="#"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
+                                        <li><a href="#"><i class="fa fa-circle-o"></i> Boxed</a></li>
+                                        <li><a href="#"><i class="fa fa-circle-o"></i> Fixed</a></li>
+                                        <li><a href="#"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-th"></i> <span>Widgets</span> <small class="label pull-right bg-green">new</small>
+                                    </a>
+                                </li>
+                                <li class="treeview">
+                                    <a href="#">
+                                        <i class="fa fa-pie-chart"></i><span>Charts</span><i class="fa fa-angle-left pull-right"></i>
+                                    </a>
+                                    <ul class="treeview-menu">
+                                        <li><a href="#"><i class="fa fa-circle-o"></i> ChartJS</a></li>
+                                        <li><a href="#"><i class="fa fa-circle-o"></i> Morris</a></li>
+                                        <li><a href="#"><i class="fa fa-circle-o"></i> Flot</a></li>
+                                        <li><a href="#"><i class="fa fa-circle-o"></i> Inline charts</a></li>
+                                    </ul>
+                                </li>
+                                <li class="treeview">
+                                    <a href="#">
+                                        <i class="fa fa-share"></i> <span>Multilevel</span>
+                                        <i class="fa fa-angle-left pull-right"></i>
+                                    </a>
+                                    <ul class="treeview-menu">
+                                        <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
+                                        <li>
+                                            <a href="#"><i class="fa fa-circle-o"></i> Level One <i class="fa fa-angle-left pull-right"></i></a>
+                                            <ul class="treeview-menu">
+                                                <li><a href="#"><i class="fa fa-circle-o"></i> Level Two</a></li>
+                                                <li>
+                                                    <a href="#"><i class="fa fa-circle-o"></i> Level Two <i class="fa fa-angle-left pull-right"></i></a>
+                                                    <ul class="treeview-menu">
+                                                        <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
+                                                        <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+                            <?php endif; ?>
                             <li class="header">LABELS</li>
                             <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
                             <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
                             <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
+                            
                         </ul>
                     </section>
                 </aside>
